cb6a6bb5162d20834e87374ac0348e6458dd5961
dmuthuri <muthuridennis@gmail.com>
2015-09-25 03:11:27 +0300

    WIP: Refactored entire chatbox into separate components

--- js/main-es6.js
+++ js/main-es6.js
@@ -1,3 +1,99 @@
+// React code
+class Header extends React.Component {
+	render(){
+		let title = 'Chicken House Help';
+		return(
+			<section className="header">
+				<h3 className="title">{title}</h3>
+				<div className="window-controls">
+					<span className="expand">+</span>
+					<span className="minimize">-</span>
+				</div>
+			</section>
+		);
+	}
+}
+
+class ChatMessageList extends React.Component {
+	constructor(){
+		super();
+		this.state = {
+			chatMessages: [],
+			chatMessage: {
+				recepient: (message) =>{
+					<li className="recepient">
+						<img src="/images/avatars/female-avatar-1.png" className="avatar" alt="recepient avatar"/>
+						<span className="message">{message}</span>
+					</li>
+				},
+				sender: (message) =>{
+					<li className="sender">
+	                    <span className="message offset-right">{message}</span>
+	                </li>
+				}
+			}
+		}
+	}
+
+	render(){
+		let chatMessageQue = this.state.chatMessages.push(this.state.chatMessage[this.props.messageType](this.props.messageText));
+		console.log(typeof(this.state.chatMessages));
+		return(
+		  	<ul className="no-style messages" id="messages-container">
+				{chatMessageQue}
+			</ul>
+		);
+	}
+}
+
+class ChatForm extends React.Component {
+	// when a user presses enter the message gets appended to the chat message list
+	updateChatMessageList(event){
+		// 13 is the Enter keycode
+		if (event.keyCode === 13) {
+			<ChatMessageList messageText={event.target.value} messageType={'sender'} />
+			event.target.value = "";
+		};
+	}
+
+	render(){
+		return(
+			<div className="footer">
+				<form action="" method="post" id="chat-message-form">
+					<textarea placeholder="Hi. I would like to receive help on ..." onKeyDown={this.updateChatMessageList} name="message"></textarea>
+				</form>
+			</div>
+		);
+	}
+}
+
+class ChatBoxBody extends React.Component {
+	render(){
+		let message = 'Hi there. My name is Jane and I\'m here to help with any questions you might have regarding our service.',
+			type = 'recepient';
+		return(
+			<section className="body">
+				<ChatMessageList messageText={message} messageType={type} />
+				<ChatForm/>
+			</section>
+		);
+	}
+}
+
+class ChatBox extends React.Component {
+	render(){
+		return(
+			<aside id="chat-box">
+				<Header/>
+				<ChatBoxBody/>
+			</aside>
+		);
+	}
+}
+
+React.render(<ChatBox/>, document.body);
+
+// wait for elements to load before attaching event handlers
 (function($) {
 	let minimizeBtn = $('.window-controls .minimize');
 	let maximizeBtn = $('.window-controls .expand');
@@ -11,35 +107,3 @@
 		chatBoxBody.slideDown();
 	})
 })(jQuery);
-
-// React code
-class ChatMessage extends React.Component {
-	constructor(){
-		super();
-		this.state = {
-			message: {
-				type: 'recepient',
-				text: 'Hi there. My name is Jane and I\'m here to help with any questions you might have regarding our service.'
-			}
-		}
-	}
-
-	render() {
-		if (this.state.message.type == 'recepient') {
-			return(
-				<li className="recepient">
-					<img src="/images/avatars/female-avatar-1.png" className="avatar" alt="recepient avatar"/>
-					<span className="message">{this.state.message.text}</span>
-				</li>
-			);
-		} else{
-			return(
-				<li className="sender">
-                    <span className="message offset-right">{'Hi Paul. I don\'t understand your FAQ'}</span>
-                </li>
-			);
-		}
-	}
-}
-
-React.render(<ChatMessage/>, document.getElementById('messages-container'));
--- js/main.js
+++ js/main.js
@@ -1,13 +1,200 @@
-'use strict';
+// React code
+"use strict";
 
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
-var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; desc = parent = getter = undefined; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; desc = parent = getter = undefined; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; continue _function; } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
+var Header = (function (_React$Component) {
+	_inherits(Header, _React$Component);
+
+	function Header() {
+		_classCallCheck(this, Header);
+
+		_get(Object.getPrototypeOf(Header.prototype), "constructor", this).apply(this, arguments);
+	}
+
+	_createClass(Header, [{
+		key: "render",
+		value: function render() {
+			var title = 'Chicken House Help';
+			return React.createElement(
+				"section",
+				{ className: "header" },
+				React.createElement(
+					"h3",
+					{ className: "title" },
+					title
+				),
+				React.createElement(
+					"div",
+					{ className: "window-controls" },
+					React.createElement(
+						"span",
+						{ className: "expand" },
+						"+"
+					),
+					React.createElement(
+						"span",
+						{ className: "minimize" },
+						"-"
+					)
+				)
+			);
+		}
+	}]);
+
+	return Header;
+})(React.Component);
+
+var ChatMessageList = (function (_React$Component2) {
+	_inherits(ChatMessageList, _React$Component2);
+
+	function ChatMessageList() {
+		_classCallCheck(this, ChatMessageList);
+
+		_get(Object.getPrototypeOf(ChatMessageList.prototype), "constructor", this).call(this);
+		this.state = {
+			chatMessages: [],
+			chatMessage: {
+				recepient: function recepient(message) {
+					React.createElement(
+						"li",
+						{ className: "recepient" },
+						React.createElement("img", { src: "/images/avatars/female-avatar-1.png", className: "avatar", alt: "recepient avatar" }),
+						React.createElement(
+							"span",
+							{ className: "message" },
+							message
+						)
+					);
+				},
+				sender: function sender(message) {
+					React.createElement(
+						"li",
+						{ className: "sender" },
+						React.createElement(
+							"span",
+							{ className: "message offset-right" },
+							message
+						)
+					);
+				}
+			}
+		};
+	}
+
+	_createClass(ChatMessageList, [{
+		key: "render",
+		value: function render() {
+			var chatMessageQue = this.state.chatMessages.push(this.state.chatMessage[this.props.messageType](this.props.messageText));
+			console.log(typeof this.state.chatMessages);
+			return React.createElement(
+				"ul",
+				{ className: "no-style messages", id: "messages-container" },
+				chatMessageQue
+			);
+		}
+	}]);
+
+	return ChatMessageList;
+})(React.Component);
+
+var ChatForm = (function (_React$Component3) {
+	_inherits(ChatForm, _React$Component3);
+
+	function ChatForm() {
+		_classCallCheck(this, ChatForm);
+
+		_get(Object.getPrototypeOf(ChatForm.prototype), "constructor", this).apply(this, arguments);
+	}
+
+	_createClass(ChatForm, [{
+		key: "updateChatMessageList",
+
+		// when a user presses enter the message gets appended to the chat message list
+		value: function updateChatMessageList(event) {
+			// 13 is the Enter keycode
+			if (event.keyCode === 13) {
+				React.createElement(ChatMessageList, { messageText: event.target.value, messageType: 'sender' });
+				event.target.value = "";
+			};
+		}
+	}, {
+		key: "render",
+		value: function render() {
+			return React.createElement(
+				"div",
+				{ className: "footer" },
+				React.createElement(
+					"form",
+					{ action: "", method: "post", id: "chat-message-form" },
+					React.createElement("textarea", { placeholder: "Hi. I would like to receive help on ...", onKeyDown: this.updateChatMessageList, name: "message" })
+				)
+			);
+		}
+	}]);
+
+	return ChatForm;
+})(React.Component);
+
+var ChatBoxBody = (function (_React$Component4) {
+	_inherits(ChatBoxBody, _React$Component4);
+
+	function ChatBoxBody() {
+		_classCallCheck(this, ChatBoxBody);
+
+		_get(Object.getPrototypeOf(ChatBoxBody.prototype), "constructor", this).apply(this, arguments);
+	}
+
+	_createClass(ChatBoxBody, [{
+		key: "render",
+		value: function render() {
+			var message = 'Hi there. My name is Jane and I\'m here to help with any questions you might have regarding our service.',
+			    type = 'recepient';
+			return React.createElement(
+				"section",
+				{ className: "body" },
+				React.createElement(ChatMessageList, { messageText: message, messageType: type }),
+				React.createElement(ChatForm, null)
+			);
+		}
+	}]);
+
+	return ChatBoxBody;
+})(React.Component);
+
+var ChatBox = (function (_React$Component5) {
+	_inherits(ChatBox, _React$Component5);
+
+	function ChatBox() {
+		_classCallCheck(this, ChatBox);
+
+		_get(Object.getPrototypeOf(ChatBox.prototype), "constructor", this).apply(this, arguments);
+	}
+
+	_createClass(ChatBox, [{
+		key: "render",
+		value: function render() {
+			return React.createElement(
+				"aside",
+				{ id: "chat-box" },
+				React.createElement(Header, null),
+				React.createElement(ChatBoxBody, null)
+			);
+		}
+	}]);
+
+	return ChatBox;
+})(React.Component);
+
+React.render(React.createElement(ChatBox, null), document.body);
+
+// wait for elements to load before attaching event handlers
 (function ($) {
 	var minimizeBtn = $('.window-controls .minimize');
 	var maximizeBtn = $('.window-controls .expand');
@@ -21,53 +208,3 @@ function _inherits(subClass, superClass) { if (typeof superClass !== 'function' 
 		chatBoxBody.slideDown();
 	});
 })(jQuery);
-
-// React code
-
-var ChatMessage = (function (_React$Component) {
-	_inherits(ChatMessage, _React$Component);
-
-	function ChatMessage() {
-		_classCallCheck(this, ChatMessage);
-
-		_get(Object.getPrototypeOf(ChatMessage.prototype), 'constructor', this).call(this);
-		this.state = {
-			message: {
-				type: 'recepient',
-				text: 'Hi there. My name is Jane and I\'m here to help with any questions you might have regarding our service.'
-			}
-		};
-	}
-
-	_createClass(ChatMessage, [{
-		key: 'render',
-		value: function render() {
-			if (this.state.message.type == 'recepient') {
-				return React.createElement(
-					'li',
-					{ className: 'recepient' },
-					React.createElement('img', { src: '/images/avatars/female-avatar-1.png', className: 'avatar', alt: 'recepient avatar' }),
-					React.createElement(
-						'span',
-						{ className: 'message' },
-						this.state.message.text
-					)
-				);
-			} else {
-				return React.createElement(
-					'li',
-					{ className: 'sender' },
-					React.createElement(
-						'span',
-						{ className: 'message offset-right' },
-						'Hi Paul. I don\'t understand your FAQ'
-					)
-				);
-			}
-		}
-	}]);
-
-	return ChatMessage;
-})(React.Component);
-
-React.render(React.createElement(ChatMessage, null), document.getElementById('messages-container'));
